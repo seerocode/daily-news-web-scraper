e3faab7da1cfa544929fdfc4ce3ecce0c3e6b8cd
seerocode <rosemarie.liriano@gmail.com>
2017-04-08 12:57:52 -0400

    extended web scrape to subsequent pages

--- BXGunScrape.py
+++ BXGunScrape.py
@@ -23,8 +23,6 @@ for link in soup_search.find_all('a'):
 appended_links = ['http://www.nydailynews.com{0}'.format(l) for l in tmp_news_links]
 #add pagination to new array
 news_search_pagination = [x for x in appended_links if '&page=' in x]
-# Remove page 1 link from search results
-news_links = [y for y in appended_links if not '&page=' in y]
 
 def remove_duplicates(news_search_pagination):
     output = []
@@ -39,6 +37,37 @@ def remove_duplicates(news_search_pagination):
 
 pagination_result = remove_duplicates(news_search_pagination)
 
+pagination_result_sf = [r.replace('bronx, gun', 'bronx%2C+gun') for r in pagination_result]
+
+new_page_links = []
+
+### ADD RESULUST FROM MORE PAGES ###
+for page in pagination_result_sf:
+	get_result = requests.get(page)
+	paged_results = BeautifulSoup(get_result.content, 'html.parser')
+	paged_soup_search = paged_results.find('div', class_='rtww')
+	for l in paged_soup_search.find_all('a'):
+		new_page_links.append(l.get('href'))
+
+### KEEP GETTING MORE PAGES ###
+# matching = [s for s in new_page_links if '&page=' in s]
+
+# for pages in new_page_links:
+# 	while matching:
+# 		get_result = requests.get(page)
+# 		paged_results = BeautifulSoup(get_result.content, 'html.parser')
+# 		paged_soup_search = paged_results.find('div', class_='rtww')
+# 		for l in paged_soup_search.find_all('a'):
+# 			new_page_links.append(l.get('href'))
+
+
+new_page_links_appended = ['http://www.nydailynews.com{0}'.format(l) for l in new_page_links]
+new_page_result_sf = [r.replace('bronx, gun', 'bronx%2C+gun') for r in new_page_links_appended]
+merged_list = new_page_result_sf + appended_links
+# Remove page 1 link from search results
+news_links = [y for y in merged_list if not '&page=' in y]
+
+#### WRITE TO EXCEL FILE ####
 result_file = open(os.path.abspath('BXVR.csv'), 'wb')
 
 with result_file as f:
