f9ade6f869402325714bfb854658fcb6aa9b023c
seerocode <rosemarie.liriano@gmail.com>
2017-04-08 08:41:05 -0400

    change variable names to be more meaningful

--- BXGunScrape.py
+++ BXGunScrape.py
@@ -6,26 +6,26 @@ import requests
 from bs4 import BeautifulSoup
 from oauth2client.service_account import ServiceAccountCredentials
 
-url = 'http://www.nydailynews.com/search-results/search-results-7.113?q=bronx%2C+gun&sortOrder=desc&selecturl=site&pdate=2016-01-01&edate=2016-12-31&tfq=articles'
-page = requests.get(url)
+news_search_url = 'http://www.nydailynews.com/search-results/search-results-7.113?q=bronx%2C+gun&sortOrder=desc&selecturl=site&pdate=2016-01-01&edate=2016-12-31&tfq=articles'
+get_page = requests.get(news_search_url)
 
-page_results = BeautifulSoup(page.content, 'html.parser')
+page_results = BeautifulSoup(get_page.content, 'html.parser')
 
 soup_search = page_results.find('div', class_='rtww')
-links = []
+tmp_news_links = []
 
 for link in soup_search.find_all('a'):
-	links.append(link.get('href'))
+	tmp_news_links.append(link.get('href'))
 	#print links
 
-new_links = ['http://www.nydailynews.com{0}'.format(s) for s in links]
+news_links = ['http://www.nydailynews.com{0}'.format(s) for s in tmp_news_links]
 
 
 result_file = open(os.path.abspath('BXVR.csv'), 'wb')
 
 with result_file as f:
 	write_to_file = csv.writer(f, dialect="excel", lineterminator='\n');
-	for val in new_links:
+	for val in news_links:
 		write_to_file.writerow([val])
 
 ### FOR WRITING TO GSPREADSHEET - WORK IN PROGRESS ###
