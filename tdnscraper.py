5ba4c916812fbd36298b89fb90423fe9275004e8
seerocode <rosemarie.liriano@gmail.com>
2017-05-08 20:40:26 -0400

    add check in case HTML for search page changes

--- tdnscraper.py
+++ tdnscraper.py
@@ -8,15 +8,15 @@ from newspaper import Article
 tmp_article_links = []
 
 for page_num in range(1, 46):
-	url = 'http://www.nydailynews.com/search-results/search-results-7.113?q=bronx,%20gun&selecturl=site&sortOrder=desc&pdate=2014-01-01&edate=2014-12-31&tfq=articles&afq=&page=' + str(page_num)
+	url = 'http://www.nydailynews.com/search-results/search-results-7.113?q=bronx,%20gun&selecturl=site&sortOrder=desc&pdate=2013-01-01&edate=2013-12-31&tfq=articles&afq=&page=' + str(page_num)
 	get_page = requests.get(url)
 	page_results = BeautifulSoup(get_page.content, 'html.parser')
 	soup_search = page_results.find('div', class_='rtww')
-
-	# Scrape for current page
-	for link in soup_search.find_all('a'):
-		tmp_article_links.append(link.get('href'))
-		#print tmp_article_links
+	if soup_search:
+		# Scrape for current page and add to temporary links array
+		for link in soup_search.find_all('a'):
+			tmp_article_links.append(link.get('href'))
+			#print tmp_article_links
 
 
 # Prepends daily news url to links
@@ -39,9 +39,9 @@ for art_link in news_links:
 		article.parse()
 		article.nlp()
 		tmp_article_details = {
-			'link': article.url,
 			'title': article.title,
 			'keywords': article.keywords,
+			'link': article.url,
 		}
 		napi_links.append(tmp_article_details)
 	except Exception:
@@ -54,7 +54,7 @@ for art_link in news_links:
 
 result_file = open(os.path.abspath('BXVR.csv'), 'w')
 
-csv_columns = ['link', 'title', 'keywords']
+csv_columns = ['title', 'keywords', 'link']
 
 with result_file as f:
 	write_to_file = csv.DictWriter(f, dialect="excel", lineterminator='\n', fieldnames=csv_columns);
