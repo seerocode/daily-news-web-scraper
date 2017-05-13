d7d801494b8a0cdca15ba241b96b9e8ab1832e1d
seerocode <rosemarie.liriano@gmail.com>
2017-05-13 17:03:12 -0400

    defined methods for functions inside scraper

--- tdnscraper.py
+++ tdnscraper.py
@@ -8,13 +8,18 @@ from newspaper import Article
 # Placeholder for links
 tmp_article_links = []
 
+# Create Dictionary
+news_api_links = []
+tmp_article_details = {}
+
 # Sets the year we are searching for in the url
 def set_search_url(year):
 	tdnews_url = 'http://www.nydailynews.com/search-results/search-results-7.113?q=bronx,%20gun&selecturl=site&sortOrder=desc&pdate=' + str(year) + '-01-01&edate=' + str(year) + '-12-31&tfq=articles&afq=&page='
 	return tdnews_url
 
+# Scrapes URL search page for links
 def tdn_scrape(url):
-	for page_num in range(1,2):
+	for page_num in range(1,48):
 		t0 = time.time()
 		headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
 		get_page = requests.get(url, headers=headers)
@@ -30,59 +35,50 @@ def tdn_scrape(url):
 		response_delay = time.time() - t0 # be polite!
 		time.sleep(10*response_delay)  # wait 10x longer than it took site to respond
 
-def main():
-	search_year = input("Please enter the year to search for: ")
-	url = set_search_url(search_year)
-	tdn_scrape(url)
-	#print tmp_article_links
+search_year = input("Please enter the year to search for: ")
+url = set_search_url(search_year)
+tdn_scrape(url)
+#print tmp_article_links
 
-	# Prepends daily news url to links
-	article_links = ['http://www.nydailynews.com{0}'.format(l) for l in tmp_article_links]
-	#print article_links
+# Prepends daily news url to links
+article_links = ['http://www.nydailynews.com{0}'.format(l) for l in tmp_article_links]
+#print article_links
 
-	# Remove paginated links
-	news_links = [y for y in article_links if not '&page=' in y]
-	print news_links
+# Remove paginated links
+news_links = [y for y in article_links if not '&page=' in y]
+#print news_links
 
+# Passes links into newspaper api to create dict of titles, keywords, and urls
+def newspaper_api_dict():
+	for article_link in news_links:
+		article = Article(article_link)
+		try:
+			article.download()
+			article.parse()
+			article.nlp()
+			tmp_article_details = {
+				'title': article.title,
+				'keywords': article.keywords,
+				'link': article.url,
+			}
+			news_api_links.append(tmp_article_details)
+		except Exception:
+			print ('we will continue after this short error')
+			continue
 
-main()
+	#print news_api_links
 
+newspaper_api_dict()
 
+# Writes dictionary to CSV file
+def write_to_file():
+	result_file = open(os.path.abspath('BXVR.csv'), 'w')
+	csv_columns = ['title', 'keywords', 'link']
 
+	with result_file as f:
+		write_to_file = csv.DictWriter(f, dialect="excel", lineterminator='\n', fieldnames=csv_columns);
+		write_to_file.writeheader()
+		for value in news_api_links:
+				write_to_file.writerow(value)
 
-
-
-# # Create Dictionary
-# napi_links = []
-# tmp_article_details = {}
-
-
-# for art_link in news_links:
-# 	article = Article(art_link)
-# 	try:
-# 		article.download()
-# 		article.parse()
-# 		article.nlp()
-# 		tmp_article_details = {
-# 			'title': article.title,
-# 			'keywords': article.keywords,
-# 			'link': article.url,
-# 		}
-# 		napi_links.append(tmp_article_details)
-# 	except Exception:
-# 		print ('we will continue after this short error')
-# 		continue
-
-# #print napi_links
-
-# #### WRITE TO EXCEL FILE ####
-
-# result_file = open(os.path.abspath('BXVR.csv'), 'w')
-
-# csv_columns = ['title', 'keywords', 'link']
-
-# with result_file as f:
-# 	write_to_file = csv.DictWriter(f, dialect="excel", lineterminator='\n', fieldnames=csv_columns);
-# 	write_to_file.writeheader()
-# 	for value in napi_links:
-# 			write_to_file.writerow(value)
+write_to_file()
